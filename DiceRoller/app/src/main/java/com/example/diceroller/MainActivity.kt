73a9d1eb656b022424b4b06b3b587417caee3afe
Sudhanshu Tripathi <sudhanshutripathi0888@gmail.com>
2022-07-29 17:46:42 +0530

    App

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : AppCompatActivity() {
         var rollbutton :Button  = findViewById(R.id.rollButton)
         rollbutton.text ="Lets Roll"
         rollbutton.setOnClickListener {
+            Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
             rollDice()
         }
         diceImage =findViewById(R.id.dice_Image)
@@ -24,10 +25,10 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice() {
 
-       // val randomInt = Random().nextInt(6)+1 -> by clicking on inline vaariable we can set whole variable inside when only
+       // val randomInt = Random().nextInt(6)+1 -> by clicking on inline variable we can set whole variable inside when only
 
 
-        val drawableResourse= when(Random().nextInt(6)+1)
+        val drawableResourse = when(Random().nextInt(6)+1)
         {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
