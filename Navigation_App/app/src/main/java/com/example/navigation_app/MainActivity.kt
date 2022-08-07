ce6321ddb87b91c811ca0ce97a3dea27b3e1a1bb
Sudhanshu Tripathi <sudhanshutripathi0888@gmail.com>
2022-08-08 02:30:24 +0530

    App

--- Navigation_App/app/src/main/java/com/example/navigation_app/MainActivity.kt
+++ Navigation_App/app/src/main/java/com/example/navigation_app/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.navigation_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.navigation_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        @Suppress("UNUSED_VARIABLE")
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 }
