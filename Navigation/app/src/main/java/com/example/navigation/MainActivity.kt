834137fe53da0c4b6c05ea14af34f7e38d79a047
Sudhanshu Tripathi <sudhanshutripathi0888@gmail.com>
2022-08-11 23:33:02 +0530

    App

--- Navigation/app/src/main/java/com/example/navigation/MainActivity.kt
+++ Navigation/app/src/main/java/com/example/navigation/MainActivity.kt
@@ -8,7 +8,7 @@ import com.example.navigation.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
 
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
--- Navigation/app/src/main/java/com/example/navigation/TitleFragment.kt
+++ Navigation/app/src/main/java/com/example/navigation/TitleFragment.kt
@@ -5,16 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.databinding.DataBindingUtil
+import com.example.navigation.databinding.FragmentTitleBinding
 
 
 class TitleFragment : Fragment()
 {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+            savedInstanceState: Bundle?): View? {
+        val binding : FragmentTitleBinding =DataBindingUtil.inflate(
+            inflater, R.layout.fragment_title, container, false
+        )
+        return binding.root
     }
 
 
