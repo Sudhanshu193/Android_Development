74cf41ff915f73571eee0a3f2014e1ce05848f2b
Sudhanshu Tripathi <sudhanshutripathi0888@gmail.com>
2022-08-23 23:55:05 +0530

    App

--- LoginPage/app/src/main/java/com/example/loginpage/MainActivity.kt
+++ LoginPage/app/src/main/java/com/example/loginpage/MainActivity.kt
@@ -1,11 +1,54 @@
 package com.example.loginpage
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import com.example.loginpage.databinding.ActivityMainBinding
+import com.example.loginpage.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+      //  var newUser = findViewById<TextView>(R.id.gotoRegister)
+        binding.Register.setOnClickListener {
+        val intent = Intent( this, register:: class.java)
+            startActivity(intent)
+        }
+        binding.forgotPass.setOnClickListener {
+            val intent = Intent(this, PasswordForgot::class.java)
+            startActivity(intent)
+        }
+        binding.btnLogin.setOnClickListener {
+            val email = binding.inputEmail.text.toString()
+            val password = binding.TextPassword.text.toString()
+
+            if(email.isNotEmpty() && password.isNotEmpty())
+            {
+
+                firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener{
+                    if(it.isSuccessful) {
+                        val intent = Intent(this, homePage::class.java)
+                        startActivity(intent)
+                    }else {
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }else{
+                Toast.makeText(this, "Enter the Field", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
     }
 }
