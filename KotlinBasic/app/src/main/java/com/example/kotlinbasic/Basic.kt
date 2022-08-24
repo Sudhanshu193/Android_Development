f2b889d298f5e835f05e40872e4065a73a220c00
Sudhanshu Tripathi <sudhanshutripathi0888@gmail.com>
2022-08-24 23:15:08 +0530

    App

---
+++ KotlinBasic/app/src/main/java/com/example/kotlinbasic/Basic.kt
@@ -0,0 +1,12 @@
+package com.example.kotlinbasic
+
+fun main(){
+    // immutable variable
+    val name = "sudhanshu"
+    println("hello " + name)
+
+    var age = 20
+    // here we 
+    var float : Float = 20.3F
+    println(float + age)
+}
